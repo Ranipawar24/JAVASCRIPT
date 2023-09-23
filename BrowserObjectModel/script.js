36768b9284363b7961be94a72ca97ff4be76f25c
Rani Pawar <pawar.rani241996@gmail.com>
2023-09-23 21:48:27 +0530

    commit

--- BrowserObjectModel/script.js
+++ BrowserObjectModel/script.js
@@ -1,57 +1,154 @@
 /* This is the example of window */
 
-window.alert("This is learnvern");
-alert("This is learnVern again");
+// window.alert("This is learnvern");
+// alert("This is learnVern again");
 
-// <!--  Example of alert -->
+// // <!--  Example of alert -->
 
-function message() {
-  alert("This is LearnVern");
-}
+// function message() {
+//   alert("This is LearnVern");
+// }
 
-// <!--  Example of confirm -->
-function message1() {
-  var temp = confirm("Are you Sure??");
+// // <!--  Example of confirm -->
+// function message1() {
+//   var temp = confirm("Are you Sure??");
 
-  if (temp == true) {
-    alert("the value is true");
-  } else {
-    alert("the value is false");
-  }
-}
+//   if (temp == true) {
+//     alert("the value is true");
+//   } else {
+//     alert("the value is false");
+//   }
+// }
 
-// <!--  Example of prompt -->
-function message2() {
-  var temp1 = prompt("Whats is your age?");
-  alert("users age is : " + temp1);
-}
+// // <!--  Example of prompt -->
+// function message2() {
+//   var temp1 = prompt("Whats is your age?");
+//   alert("users age is : " + temp1);
+// }
 
-// <!--  Example of Open -->
-function message3() {
-  var temp2 = open(
-    "https://my.newtonschool.co/course/augybj9bee3q/details?course-hash=88v2rxm2qknh&tab=course&activityTypeTab=assignment&filter_page=3"
-  );
-  // alert("users age is : "+temp1);
-}
+// // <!--  Example of Open -->
+// function message3() {
+//   var temp2 = open(
+//     "https://my.newtonschool.co/course/augybj9bee3q/details?course-hash=88v2rxm2qknh&tab=course&activityTypeTab=assignment&filter_page=3"
+//   );
+//   // alert("users age is : "+temp1);
+// }
 
-// <!--  Example of setTimeOut -->
-function message4() {
-  setTimeout(function () {
-    alert("this is Rani");
-  }, 3000);
-}
+// // <!--  Example of setTimeOut -->
+// function message4() {
+//   setTimeout(function () {
+//     alert("this is Rani");
+//   }, 3000);
+// }
 
-// <!--  Example of close -->
-function message5() {
-  close();
-}
+// // <!--  Example of close -->
+// function message5() {
+//   close();
+// }
 
-/* This is the example of History */
+// /* This is the example of History */
 
-function goBack() {
-  history.back();
-}
+// function goBack() {
+//   history.back();
+// }
 
-function goForward() {
-  history.forward();
-}
+// function goForward() {
+//   history.forward();
+// }
+
+// ........Operatores................
+// let a = 10;
+// let b = 4;
+// console.log(++a);
+// console.log(a++);
+// console.log(a--);
+// console.log(--a);
+// console.log(a);
+
+// let x = prompt("Hey there beauty");
+// a = Number.parseInt(x);
+// if (x > 0) {
+//   alert("this is a valid age");
+// }
+
+// let age = prompt("What is your age?");
+
+// if (age > 10 && age < 20) {
+//   console.log("age lies between 10 and 20");
+// } else {
+//   console.log("age doesnt lies");
+//   12;
+// }
+
+//que 1 switch case ?
+//switch case12
+// let age = prompt("What is your age?");
+// switch (age) {
+//   case "12":
+//     console.log("your age is 12");
+//     break;
+//   case "14":
+//     console.log("your age is 14");
+//     break;
+//   case "13":
+//     console.log("your age is 13");
+//     break;
+
+//   case "15":
+//     console.log("your age is 15");
+//     break;
+
+//   default:
+//     console.log("your age is not special");
+// }
+
+//2 number is divisible by 2 and 3?
+
+// let num = prompt("What is your age?");
+// num = Number.parseInt(num);
+// if (num % 2 == 0 && num % 3 == 0) {
+//   console.log("number divisible by 2 and 3");
+// } else {
+//   console.log("number is not divisible by 2 and 3");
+// }
+
+//3 print you can drive or you cannot drive based on age being greater than 18 using ternary operator?
+// let age = 2;
+// let x = age > 18 ? "You can drive" : "You cannot drive";
+// console.log(x);
+
+//............. LOOPS.............//
+// for loop............
+
+// let sum = 0;
+// let n = prompt("Enter the value of n");
+// n = Number.parseInt(n);
+// for (let i = 0; i < n; i++) {
+//   sum += i + 1;
+// }
+// console.log("sum of first" + n + "natural num is " + sum);
+
+//for in loop............
+
+// let obj = {
+//   rani: 90,
+//   pani: 50,
+//   anuja: 45,
+//   angel: 60,
+// };
+// for (let a in obj) {
+//   console.log("Mark of " + a + " are = " + obj[a]);
+// }
+
+//for of loop............
+
+//While loop............
+// let n = prompt("Enter the value of n");
+// n = Number.parseInt(n);
+// let i = 0;
+// while (i < n) {
+//   console.log(i);
+//   i++;
+// }
+
+//PRACTICE LOOPS , FUNCTIONS..........
