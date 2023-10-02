25993e3ded74722d09a5d624e3f479caa57edb39
Rani Pawar <pawar.rani241996@gmail.com>
2023-10-02 12:02:28 +0530

    update array&string

---
+++ Arrays/script.js
@@ -0,0 +1,10 @@
+let marks = [52, 63, 85, 91, false, "Not present"];
+console.log(marks);
+console.log(0);
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+//.................................||..................................//
---
+++ String/script.js
@@ -0,0 +1,57 @@
+// let name = "Rani";
+// // console.log(name.length);
+// console.log(name[0]);
+// console.log(name[1]);
+// console.log(name[2]);
+// console.log(name);
+//.................................||..................................//
+
+// //... Template Literal...
+// let boy1 = "Ankit";
+// let boy2 = "Yash";
+// //Ankit is a friend of Yash
+// let sentence = `${boy1} is a friend of ${boy2}`;
+// console.log(sentence);
+//.................................||..................................//
+
+//..... Escape sequence Characters...
+let fruit = "Bana'na";
+console.log(fruit);
+// \n for next line/ new line
+
+//.................................||..................................//
+// Uppercase and Lowercase...
+let name = "Rani";
+// console.log(name.toUpperCase()); // convert all characters into Capital
+// console.log(name.toLowerCase()); // convert all characters into Small
+//.................................||..................................//
+
+// //....String Methods
+// console.log(name.slice(2, 4)); //
+// console.log(name.slice(2)); //
+// console.log(name.replace("Ra", "Pa")); //to replace a char rani to pani
+// console.log(name.slice(2)); //
+
+///.................................||..................................//
+
+// //Concat
+// let frend = "Anuja";
+// console.log(name.concat(" is a friend of ", frend, " ok"));
+
+// //.................................||..................................//
+
+// // trim
+// let frend2 = "          Tanji        ";
+// console.log(frend2.trim()); // trim removes a whiteSapces
+
+//.................................||..................................//
+let myString = "hey, there!";
+let num = 7;
+for (let i = 0; i < num; i++) {
+  console.log(myString);
+}
+
+let str = 'har"';
+console.log(str.length);
+
+//.................................||..................................//
