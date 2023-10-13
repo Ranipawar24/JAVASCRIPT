57655d6cd0b1bc3eac97c7b4e42b45ab62af9f4d
Rani Pawar <pawar.rani241996@gmail.com>
2023-10-13 12:21:59 +0530

    update

--- String/script.js
+++ String/script.js
@@ -15,13 +15,13 @@
 //.................................||..................................//
 
 //..... Escape sequence Characters...
-let fruit = "Bana'na";
-console.log(fruit);
+// let fruit = "Bana'na";
+// console.log(fruit);
 // \n for next line/ new line
 
 //.................................||..................................//
 // Uppercase and Lowercase...
-let name = "Rani";
+// let name = "Rani";
 // console.log(name.toUpperCase()); // convert all characters into Capital
 // console.log(name.toLowerCase()); // convert all characters into Small
 //.................................||..................................//
@@ -45,13 +45,55 @@ let name = "Rani";
 // console.log(frend2.trim()); // trim removes a whiteSapces
 
 //.................................||..................................//
-let myString = "hey, there!";
-let num = 7;
-for (let i = 0; i < num; i++) {
-  console.log(myString);
-}
+// let myString = "hey, there!";
+// let num = 7;
+// for (let i = 0; i < num; i++) {
+//   console.log(myString);
+// }
 
-let str = 'har"';
-console.log(str.length);
+// let str = 'har"';
+// console.log(str.length);
 
 //.................................||..................................//
+
+const name = "Rani";
+const repoCount = 50;
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String("Ranip");
+// console.log(gameName.length);
+
+//.................................||..................................//
+// TOUPPERCASE
+// console.log(gameName.toUpperCase());
+
+//.................................||..................................//
+// CHARAT
+// console.log(gameName.charAt(2));
+
+//.................................||..................................//
+// INDEXOF
+// console.log(gameName.indexOf("R"));
+
+//.................................||..................................//
+// SUBSTRING
+const newString = gameName.substring(0, 3);
+console.log(newString);
+
+//.................................||..................................//
+// SLICE
+const anotherString = gameName.slice(-4, 2);
+console.log(anotherString);
+
+//.................................||..................................//
+// TRIM
+const newStringOne = "    Rani    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+//.................................||..................................//
+// REPLACE
+const url = "https://spotity.vercel.app/rani%20p";
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("app"));
