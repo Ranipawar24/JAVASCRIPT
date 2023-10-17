e7ece8956813b7994cd81872056329b3fb0c5339
Rani Pawar <pawar.rani241996@gmail.com>
2023-10-17 12:44:24 +0530

    update Array

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,10 +1,38 @@
-let marks = [52, 63, 85, 91, false, "Not present"];
-console.log(marks);
-console.log(0);
-console.log(1);
-console.log(2);
-console.log(3);
-console.log(4);
-console.log(5);
+const marks = [52, 63, 85, 91, false, "Not present"];
+const myArr = ["sonu", "monu"];
+const myArr2 = new Array(1, 2, 4, 6);
+console.log(myArr[0]);
+// console.log(marks);
+// console.log(0);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+//.................................||..................................//
+
+// Array Methods
+
+// myArr2.push(7); // foradd
+// console.log(myArr2);
+// myArr2.pop(); // last value remove
+// console.log(myArr2);
+
+// myArr2.unshift(8); // add first
+// console.log(myArr2);
+
+// myArr2.shift(); // remove add
+// console.log(myArr2);
+
+// console.log(myArr2.includes(10)); // to check value present or not in a boolean format
+//.................................||..................................//
+const newArrau = myArr2.join();
+console.log(myArr2);
+console.log(typeof newArrau);
+
+//.................................||..................................//
+
+// SLICE SPLICE
 
 //.................................||..................................//
