0e221cc492a88745ffdd25cbb9dfb8fc52b286f1
Rani Pawar <pawar.rani241996@gmail.com>
2023-12-07 12:47:34 +0530

    Update script.js

--- Objects/script.js
+++ Objects/script.js
@@ -61,5 +61,6 @@ const obj1 = { 1: "a", 2: "b" };
 const obj2 = { 3: "a", 4: "b" };
 const obj4 = { 5: "a", 6: "b" };
 
-const obj3 = Object.assign({}, obj1, obj2, obj4); // {}=target, obj1 = source
+// const obj3 = Object.assign({}, obj1, obj2, obj4); // {}=target, obj1 = source
+const obj3 = { ...obj1, ...obj2, ...obj4 }; // using spread operator
 console.log(obj3);
