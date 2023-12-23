ace940ab78edbbd6ea343881951bcc22dea26337
Rani Pawar <pawar.rani241996@gmail.com>
2023-12-23 10:10:45 +0530

    Update script.js

--- Functions/script.js
+++ Functions/script.js
@@ -1,2 +1,9 @@
 //.................................||..................................//
 //Functions and Parameters
+function sayMyName() {
+  console.log("R");
+  console.log("a");
+  console.log("n");
+  console.log("i");
+}
+sayMyName();
