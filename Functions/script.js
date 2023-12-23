615a5f0ee09529820bf47a7f050e3a06ae278162
Rani Pawar <pawar.rani241996@gmail.com>
2023-12-23 10:28:11 +0530

    Update script.js

--- Functions/script.js
+++ Functions/script.js
@@ -7,3 +7,16 @@ function sayMyName() {
   console.log("i");
 }
 sayMyName();
+
+// function addTwoNumbers(num1, nun2) {//   // thisl is a Parameters
+//   console.log(num1 + nun2);
+// }
+// addTwoNumbers(3, 7); // this is a Arguments
+
+function addTwoNumbers(num1, nun2) {
+  return num1 + nun2;
+  //   let result = num1 + nun2;
+  //   return result;
+}
+const result = addTwoNumbers(3, 7);
+console.log("Result: ", result);
