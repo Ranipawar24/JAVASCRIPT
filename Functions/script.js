caa2ce2e7b901a31c6414fe1e43476aed5b5e438
Rani Pawar <pawar.rani241996@gmail.com>
2023-12-23 10:45:49 +0530

    Update script.js

--- Functions/script.js
+++ Functions/script.js
@@ -12,11 +12,24 @@ sayMyName();
 //   console.log(num1 + nun2);
 // }
 // addTwoNumbers(3, 7); // this is a Arguments
-
+//.................................||..................................//
 function addTwoNumbers(num1, nun2) {
   return num1 + nun2;
   //   let result = num1 + nun2;
   //   return result;
 }
 const result = addTwoNumbers(3, 7);
-console.log("Result: ", result);
+// console.log("Result: ", result);
+
+//.................................||..................................//
+
+// FOR USER LOGGED IN
+function loginUserMessage(username = "rANI") {
+  if (!username) {
+    console.log("Please enter a username");
+    return;
+  }
+  return `${username} just logged in`;
+}
+// console.log(loginUserMessage("Ranii"));
+console.log(loginUserMessage());
