a99b572247b2376729a705d5faab0646aeda0f00
Rani Pawar <pawar.rani241996@gmail.com>
2023-12-26 12:59:48 +0530

    update scope

---
+++ Scope/script.js
@@ -0,0 +1,17 @@
+//.................................||..................................//
+
+//GLOBAL AND LOCAL SCOPE
+
+let a = 10; // global scope
+const b = 20;
+var c = 30;
+
+if (true) {
+  // block scope
+  let a = 10;
+  const b = 20;
+  var c = 30;
+}
+// console.log(a);
+console.log(b);
+console.log(c);
