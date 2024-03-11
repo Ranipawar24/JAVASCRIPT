5be11408e2b7d0a890c52a346df15936efba861d
Rani Pawar <pawar.rani241996@gmail.com>
2024-03-11 13:13:01 +0530

    Update script.js

--- Scope/script.js
+++ Scope/script.js
@@ -30,3 +30,15 @@ function one() {
   two();
 }
 one();
+
+//................................. Interseting  .................................//
+
+console.log(addOne(5));
+function addOne(num) {
+  return num + 1;
+}
+
+console.log(addTwo(5));
+const addTwo = function (num) {
+  return num + 2;
+};
